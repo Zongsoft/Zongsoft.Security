124d46a324862e8c6f7201dd175ef2a4a6c7a36e
Popeye Zhong <zongsoft@gmail.com>
2015-07-17 21:53:15 +0800

    :boom: 初始化项目库。

---
+++ src/Membership/MembershipProviderBase.cs
@@ -0,0 +1,116 @@
+﻿/*
+ * Authors:
+ *   钟峰(Popeye Zhong) <zongsoft@gmail.com>
+ *
+ * Copyright (C) 2015 Zongsoft Corporation <http://www.zongsoft.com>
+ *
+ * This file is part of Zongsoft.Security.
+ *
+ * Zongsoft.Security is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * Zongsoft.Security is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Zongsoft.Security; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+using System;
+using System.Collections.Generic;
+
+using Zongsoft.Data;
+using Zongsoft.Options;
+
+namespace Zongsoft.Security.Membership
+{
+	public class MembershipProviderBase : MarshalByRefObject
+	{
+		#region 成员字段
+		private IDataAccess _dataAccess;
+		private ISettingsProvider _settings;
+		private string _namespace;
+		#endregion
+
+		#region 构造函数
+		protected MembershipProviderBase()
+		{
+		}
+
+		protected MembershipProviderBase(ISettingsProvider settings)
+		{
+			_settings = settings;
+		}
+		#endregion
+
+		#region 公共属性
+		public IDataAccess DataAccess
+		{
+			get
+			{
+				return _dataAccess;
+			}
+			set
+			{
+				if(value == null)
+					throw new ArgumentNullException();
+
+				_dataAccess = value;
+			}
+		}
+
+		public string Namespace
+		{
+			get
+			{
+				if(string.IsNullOrWhiteSpace(_namespace))
+				{
+					var settings = _settings;
+
+					if(settings != null)
+						_namespace = settings.GetValue("Namespace") as string;
+				}
+
+				return _namespace;
+			}
+			set
+			{
+				if(value == null)
+					_namespace = null;
+				else
+					_namespace = string.IsNullOrWhiteSpace(value) ? null : value.Trim();
+			}
+		}
+
+		public ISettingsProvider Settings
+		{
+			get
+			{
+				return _settings;
+			}
+			set
+			{
+				_settings = value;
+			}
+		}
+		#endregion
+
+		#region 保护方法
+		protected IDataAccess EnsureDataAccess()
+		{
+			if(_dataAccess == null)
+				throw new InvalidOperationException("The value of 'DataAccess' property is null.");
+
+			return _dataAccess;
+		}
+		#endregion
+	}
+}
---
+++ src/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Zongsoft.Security")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Zongsoft Corporation")]
+[assembly: AssemblyProduct("Zongsoft Security Library")]
+[assembly: AssemblyCopyright("Copyright(C) Zongsoft Corporation 2010-2015. All rights reserved.")]
+[assembly: AssemblyTrademark("Zongsoft is registered trademarks of Zongsoft Corporation in the P.R.C. and/or other countries.")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("4.0.1507.*")]
+[assembly: AssemblyFileVersion("4.0.1507.0")]
