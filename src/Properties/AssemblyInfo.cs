349aad86845b15718d138279da9ec345e7308475
Popeye Zhong <zongsoft@gmail.com>
2016-02-22 11:26:47 +0800

    :tokyo_tower: 统一调整版本号。 :vibration_mode:

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -3,14 +3,14 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Zongsoft.Security")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("This is a library about security management.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Zongsoft Corporation")]
 [assembly: AssemblyProduct("Zongsoft Security Library")]
-[assembly: AssemblyCopyright("Copyright(C) Zongsoft Corporation 2010-2015. All rights reserved.")]
+[assembly: AssemblyCopyright("Copyright(C) Zongsoft Corporation 2010-2016. All rights reserved.")]
 [assembly: AssemblyTrademark("Zongsoft is registered trademarks of Zongsoft Corporation in the P.R.C. and/or other countries.")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("4.0.1507.*")]
-[assembly: AssemblyFileVersion("4.0.1507.0")]
+[assembly: AssemblyVersion("4.1.1602.0")]
+[assembly: AssemblyFileVersion("4.1.1602.0")]
