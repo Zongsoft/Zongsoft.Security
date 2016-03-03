6365301ae52ff758bde929aa77add2e8e99cd8e9
Popeye Zhong <zongsoft@gmail.com>
2016-03-03 14:30:42 +0800

    :sun_with_face:  :fu:  为 FullName 字段赋默认值。 :name_badge:

--- docs/Zongsoft.Security-Data(MySQL).sql
+++ docs/Zongsoft.Security-Data(MySQL).sql
@@ -1,9 +1,9 @@
 # 添加系统内置角色
-INSERT INTO `Security_Role` (`RoleId`, `Name`, `Description`) VALUES (1, 'Administrators', '系统管理角色(系统内置角色)');
+INSERT INTO `Security_Role` (`RoleId`, `Name`, `FullName`, `Description`) VALUES (1, 'Administrators', 'Administrators', '系统管理角色(系统内置角色)');
 
 # 添加系统内置用户
-INSERT INTO `Security_User` (`UserId`, `Name`, `Description`) VALUES (1, 'Administrator', '系统管理员(系统内置帐号)');
-INSERT INTO `Security_User` (`UserId`, `Name`, `Description`) VALUES (2, 'Guest', '；来宾(系统内置帐号)');
+INSERT INTO `Security_User` (`UserId`, `Name`, `FullName`, `Description`) VALUES (1, 'Administrator', 'Administrator', '系统管理员(系统内置帐号)');
+INSERT INTO `Security_User` (`UserId`, `Name`, `FullName`, `Description`) VALUES (2, 'Guest', 'Guest', '来宾(系统内置帐号)');
 
 # 添加系统内置保留名字
 INSERT INTO `Security_Censorship` (`Name`, `Word`) VALUES ('Names', 'Automao');
