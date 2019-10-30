2edca3e2f61a961c4fba4600146745be7e62833b
Popeye Zhong <9555843@qq.com>
2019-10-31 02:39:54 +0800

    清理初始化数据脚本。 :cl:

--- database/Zongsoft.Security-Data.sql
+++ database/Zongsoft.Security-Data.sql
@@ -4,43 +4,10 @@ INSERT INTO Security_Role (RoleId, Name, FullName, Description) VALUES (2, 'Secu
 
 /* 添加系统内置用户 */
 INSERT INTO Security_User (UserId, Name, FullName, Description, Status) VALUES (1, 'Administrator', 'Administrator', '系统管理员(系统内置帐号)', 0);
-INSERT INTO Security_User (UserId, Name, FullName, Description, Status) VALUES (2, 'Guest', 'Guest', '来宾(系统内置帐号)', 1);
+INSERT INTO Security_User (UserId, Name, FullName, Description, Status) VALUES (2, 'Guest', 'Guest', '来宾', 1);
 
 /* 添加系统内置保留名字 */
-INSERT INTO Security_Censorship (Name, Word) VALUES ('Names', 'Automao');
 INSERT INTO Security_Censorship (Name, Word) VALUES ('Names', 'Zongsoft');
-INSERT INTO Security_Censorship (Name, Word) VALUES ('Names', 'SaaS');
-INSERT INTO Security_Censorship (Name, Word) VALUES ('Names', 'admin');
-INSERT INTO Security_Censorship (Name, Word) VALUES ('Names', 'root');
-INSERT INTO Security_Censorship (Name, Word) VALUES ('Names', 'anonymous');
-INSERT INTO Security_Censorship (Name, Word) VALUES ('Names', 'everyone');
-INSERT INTO Security_Censorship (Name, Word) VALUES ('Names', 'system');
-INSERT INTO Security_Censorship (Name, Word) VALUES ('Names', 'Global');
-INSERT INTO Security_Censorship (Name, Word) VALUES ('Names', 'Globals');
-INSERT INTO Security_Censorship (Name, Word) VALUES ('Names', 'Administrator');
-INSERT INTO Security_Censorship (Name, Word) VALUES ('Names', 'Administrators');
-INSERT INTO Security_Censorship (Name, Word) VALUES ('Names', 'Guest');
-INSERT INTO Security_Censorship (Name, Word) VALUES ('Names', 'Guests');
-INSERT INTO Security_Censorship (Name, Word) VALUES ('Names', 'User');
-INSERT INTO Security_Censorship (Name, Word) VALUES ('Names', 'Users');
-INSERT INTO Security_Censorship (Name, Word) VALUES ('Names', 'Customer');
-INSERT INTO Security_Censorship (Name, Word) VALUES ('Names', 'Customers');
-INSERT INTO Security_Censorship (Name, Word) VALUES ('Names', 'Manager');
-INSERT INTO Security_Censorship (Name, Word) VALUES ('Names', 'Managers');
-INSERT INTO Security_Censorship (Name, Word) VALUES ('Names', 'Moderator');
-INSERT INTO Security_Censorship (Name, Word) VALUES ('Names', 'Moderators');
-INSERT INTO Security_Censorship (Name, Word) VALUES ('Names', 'Editor');
-INSERT INTO Security_Censorship (Name, Word) VALUES ('Names', 'Editors');
-INSERT INTO Security_Censorship (Name, Word) VALUES ('Names', 'Demo');
-INSERT INTO Security_Censorship (Name, Word) VALUES ('Names', 'Demos');
-INSERT INTO Security_Censorship (Name, Word) VALUES ('Names', 'Test');
-INSERT INTO Security_Censorship (Name, Word) VALUES ('Names', 'Tests');
-INSERT INTO Security_Censorship (Name, Word) VALUES ('Names', 'Tester');
-INSERT INTO Security_Censorship (Name, Word) VALUES ('Names', 'Testers');
-INSERT INTO Security_Censorship (Name, Word) VALUES ('Names', 'Service');
-INSERT INTO Security_Censorship (Name, Word) VALUES ('Names', 'Services');
-INSERT INTO Security_Censorship (Name, Word) VALUES ('Names', 'Security');
-INSERT INTO Security_Censorship (Name, Word) VALUES ('Names', 'Securities');
 
 /* 添加非法关键字 */
 INSERT INTO Security_Censorship (Name, Word) VALUES ('Sensitives', 'fuck');
